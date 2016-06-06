89a8cce9677fb6e738ae122e5bf5a6989c6e238f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-06-05 20:38:31 -0400

    chore(ci): setup semantic release, close #3

--- timer-bar.js
+++ timer-bar.js
@@ -1,57 +1,57 @@
-(function (timerDurationMinutes, barColor, height) {
-  function toText(response) { return response.text(); }
+;(function (timerDurationMinutes, barColor, height) {
+  function toText (response) { return response.text() }
 
-  function loadScript(url) {
+  function loadScript (url) {
+    /* global fetch */
     return fetch(url)
       .then(toText)
-      .then(eval);
+      .then(eval) // eslint-disable-line no-eval
   }
 
-  function loadProgressBars() {
-    var url = 'https://rawgit.com/bahmutov/progress-full-width/master/bar.js';
-    return loadScript(url);
+  function loadProgressBars () {
+    var url = 'https://rawgit.com/bahmutov/progress-full-width/master/bar.js'
+    return loadScript(url)
   }
 
-  const TIMER_BAR_KEY_CODE = 84; // 't'
-  var isRunning = false; // TODO for pausing the timer bar
+  const TIMER_BAR_KEY_CODE = 84 // 't'
+  var isRunning = false // TODO for pausing the timer bar
 
-  function onKey(event) {
-    console.log('event', event);
+  function onKey (event) {
+    console.log('event', event)
     if (event.keyCode === TIMER_BAR_KEY_CODE) {
       if (isRunning) {
-        console.log('pausing timer-bar not implemented yet');
+        console.log('pausing timer-bar not implemented yet')
       } else {
-        console.log('starting timer-bar');
-        initTimer();
-        isRunning = true;
+        console.log('starting timer-bar')
+        initTimer()
+        isRunning = true
       }
     }
   }
   if (!window.__timerBarOnKey) {
-    window.__timerBarOnKey = onKey;
+    window.__timerBarOnKey = onKey
   }
 
-  function registerKey() {
-    document.removeEventListener('keyup', window.__timerBarOnKey);
-    document.addEventListener('keyup', window.__timerBarOnKey);
-    console.log('Press "t" to start the timer-bar');
+  function registerKey () {
+    document.removeEventListener('keyup', window.__timerBarOnKey)
+    document.addEventListener('keyup', window.__timerBarOnKey)
+    console.log('Press "t" to start the timer-bar')
   }
 
-  function initTimer() {
-    console.log('starting timer for %d minute(s)', timerDurationMinutes);
+  function initTimer () {
+    console.log('starting timer for %d minute(s)', timerDurationMinutes)
     var options = {
       color: barColor,
       height: height
-    };
-    progressBars.init(options)
-      .timer(timerDurationMinutes * 60);
+    }
+    window.progressBars.init(options)
+      .timer(timerDurationMinutes * 60)
   }
 
-  if (typeof progressBars === 'undefined') {
+  if (typeof window.progressBars === 'undefined') {
     loadProgressBars()
-      .then(registerKey);
+      .then(registerKey)
   } else {
-    registerKey();
+    registerKey()
   }
-
-}(45 /* minute(s) */, '#ff00ff' /* color */, 4 /* height pixels */));
+}(45 /* minute(s) */, '#ff00ff' /* color */, 4 /* height pixels */))
